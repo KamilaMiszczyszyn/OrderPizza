5c852f75549733ce78f149e8df186727ba5cc632
Kamia Miszczyszyn <miszczyszynkamila96@gmail.com>
2024-01-10 21:36:32 +0100

    Configure firebase

---
+++ OrderPizza/src/firebase/firebase.config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAHLBx5qkQYIN0Symu-46vn4l6ONiT_Mm0",
+  authDomain: "orderpizza-38fdd.firebaseapp.com",
+  projectId: "orderpizza-38fdd",
+  storageBucket: "orderpizza-38fdd.appspot.com",
+  messagingSenderId: "572084440532",
+  appId: "1:572084440532:web:5f547067da8aece19f5a41"
+};
